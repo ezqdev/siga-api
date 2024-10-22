233b99f414fe941bcd3429aa5baa7277fafd9e47
Sergio Benjamin <benqui@hotmail.es>
2024-10-22 15:03:50 -0600

    Avance 5.2

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EstateController;
 use App\Http\Controllers\PositionController;
 use App\Http\Controllers\RequisitionController;
@@ -11,18 +12,23 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::apiResource('/rol',RolController::class);
-Route::apiResource('/user',UserController::class);
-Route::apiResource('/position',PositionController::class);
-Route::apiResource('/space',SpaceController::class);
-Route::apiResource('/service',ServiceController::class);
-Route::apiResource('/estate', EstateController::class); //* agregar relacion con el estate en la requisición y reserva.
-Route::apiResource('/requisition', RequisitionController::class);
-Route::apiResource('/reservation',ReservationController::class);
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('/rol',RolController::class);
+    Route::apiResource('/user',UserController::class);
+    Route::apiResource('/position',PositionController::class);
+    Route::apiResource('/space',SpaceController::class);
+    Route::apiResource('/service',ServiceController::class);
+    Route::apiResource('/estate', EstateController::class); //* agregar relacion con el estate en la requisición y reserva.
+    Route::apiResource('/requisition', RequisitionController::class);
+    Route::apiResource('/reservation',ReservationController::class);
 
-Route::post('reservation/update/{id}', [ReservationController::class,'update']);
-Route::post('space/update/{id}', [SpaceController::class,'update']);
+    Route::post('reservation/update/{id}', [ReservationController::class,'update']);
+    Route::post('space/update/{id}', [SpaceController::class,'update']);
+});
+
+Route::post('/login',[AuthController::class,'login']);
+
